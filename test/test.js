ea8d3de948b5f9ae28f4ad06d027ef7a930fa6a5
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2018-09-24 07:10:26 +0900

    Change: Logging to show correct function tested

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ const SystemLoader = require("../systemLoader.js");
 // Checks for function failures across node-system
 try {
     // Check SystemLoader functions
-    console.log("Checking function loadYaml with dummy...")
+    console.log("Checking SystemLoader with dummy...")
     var sl = new SystemLoader.SystemLoader("./", "test/", "stars");
 } catch (err) {
     console.log(err);
