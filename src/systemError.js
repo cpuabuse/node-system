1042229622e2ce659948ba118a232ae89089b743
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2018-10-05 11:59:04 +0900

    Change: Error addition

--- src/systemError.js
+++ src/systemError.js
@@ -10,28 +10,14 @@
  * @inner
  * @memberof module:system
  * @extends external:error
- * @param {module:system.System} systemContext System context
  * @param {string} code Error code
  * @param {string} message Error message
  * @throws {external:Error} Throwing error if the code already defined
  */
 class SystemError extends Error{
-	constructor(systemContext, code, message){
-		let errorNotSet = true; // Flag if fail to get error
-
-		if(systemContext.errors.hasOwnProperty(code)){ // Check that error exists as an event
-			if (systemContext.errors[code].hasOwnProperty("error")){ // Check that event can do errors
-				if(typeof systemContext.errors[code].error === "string"){
-					super(message);
-					this.code = code;
-					errorNotSet = false;
-				}
-			}
-		}
-
-		if(errorNotSet){
-			throw new Error("System error undefined.");
-		}
+	constructor(code, message){
+		super(message);
+		this.code = code;
 	}
 
 	/**
