3ef1469cb4198ec03a45a6d58ca7f780e9ec472c
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2018-11-16 20:38:55 +0900

    Change: Events docs

--- src/events.js
+++ src/events.js
@@ -1,6 +1,9 @@
 // events.js
 /*
 	Contains list of events used in system.
+	Reasons for semantical dereference:
+		- Events are fired by string and not by object reference
+		- The centralized management of event names is convinient
 */
 "use strict";
 
@@ -12,43 +15,37 @@ module.exports = {
 	 */
 	errorExists: "error_exists",
 	/**
-	 * System load event.
-	 * Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
+	 * System load event. Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
 	 * @event module:system.System#events#systemLoad
 	 * @default system_load
 	 */
 	systemLoad: "system_load",
 	/**
-	 * System load event.
-	 * Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
+	 * Behavior has been attached.
 	 * @event module:system.System#events#behaviorAttach
 	 * @default behavior_attach
 	 */
 	behaviorAttach: "behavior_attach",
 	/**
-	 * System load event.
-	 * Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
+	 * Behavior could not be attached.
 	 * @event module:system.System#events#behaviorAttachFail
 	 * @default behavior_attach_fail
 	 */
 	behaviorAttachFail: "behavior_attach_fail",
 	/**
-	 * System load event.
-	 * Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
+	 * Incorrect behavior attach request.
 	 * @event module:system.System#events#behaviorAttachRequestFail
 	 * @default behavior_attach_request_fail
 	 */
 	behaviorAttachRequestFail: "behavior_attach_request_fail",
 	/**
-	 * System load event.
-	 * Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
+	 * Type error has been encountered.
 	 * @event module:system.System#events#typeError
 	 * @default type_error
 	 */
 	typeError: "type_error",
 	/**
-	 * System load event.
-	 * Fires at the end of system load, so it is safe to execute code in the then() directive of behavior associated.
+	 * Failed to fire an event.
 	 * @event module:system.System#events#eventFail
 	 * @default event_fail
 	 */
