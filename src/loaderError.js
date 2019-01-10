96aa185f3978317ea1b87fa0d1b5a1ca53c586f4
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2019-01-10 20:47:17 +0900

    Add: LoaderError JSDoc and and defaults

--- src/loaderError.js
+++ src/loaderError.js
@@ -4,10 +4,37 @@
 */
 "use strict";
 
+/**
+ * Extended error class for system loading errors.
+ * @inner
+ * @memberof module:system
+ * @extends external:Error
+ */
 class LoaderError extends Error{
+	/**
+	 * Creates an instance of LoaderError.
+	 * @param {string} [code=default_code] Error code
+	 * @param {string} [message=default_message] Error message
+	 */
 	constructor(code, message){
-		super(message);
-		this.code = code;
+		let defaultCode = false;
+		let defaultMessage = false;
+
+		// Determine if to use a default code
+		if(typeof code !== "string"){
+			defaultCode = true;
+		} else if(code.length === 0){
+			defaultCode = true;
+		}
+
+		// Determine if to use a default message
+		if(typeof message !== "string"){
+			defaultMessage = true;
+		} else if(code === 0){
+			defaultMessage = true;
+		}
+		super(defaultMessage ? message : "default_code");
+		this.code = defaultCode ? code : "default_message";
 	}
 }
 
