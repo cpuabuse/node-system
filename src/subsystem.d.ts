eeac212b9fda25abe5f68c24afc88d62ed0dd714
foamy-latte <35882542+foamy-latte@users.noreply.github.com>
2019-05-25 15:32:02 +0900

    Change: Pretest script

--- src/subsystem.d.ts
+++ src/subsystem.d.ts
@@ -1,12 +1,27 @@
 import { System } from "./system";
+/**
+ * Interface for subsystem constructor data exchange.
+ * @typedef ConstructorArgs
+ * @property {module:system.System} systemContext Context of a parent system.
+ * @property {Object} args Arbitrary arguments.
+ */
 export interface ConstructorArgs {
     systemContext: System;
     args: any;
 }
+/**
+ * A way methods are transfered to a subsystem.
+ * @typedef SubsystemMethod
+ * @property {string} name Name of a function.
+ * @property {Function} fn Function body, taking arbitrary arguments.
+ */
 export declare type SubsystemMethod = {
     name: string;
     fn: Function;
 };
+/**
+ * @typedef Method
+ */
 declare type Method = {
     [key: string]: Function;
 };
