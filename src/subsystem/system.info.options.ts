0f0f714cd6549bc9dea5471c3aead5b7e41357b5
Gabriele Gentile <gabriele.gentile123@gmail.com>
2019-06-16 17:44:28 +0900

    Remove: Function checkOptionsFailure

--- src/subsystem/system.info.options.ts
+++ src/subsystem/system.info.options.ts
@@ -7,49 +7,16 @@
  * Used for storing system options.
  */
 
-import { SubsystemExtensionArgs } from "../system/subsystem"; /* eslint-disable-line no-unused-vars */ // ESLint type import detection bug
+import { Access, SubsystemExtensionArgs } from "../system/subsystem"; /* eslint-disable-line no-unused-vars */ // ESLint type import detection bug
 import Info from "./system.info";
 import { LoaderError } from "../loaderError";
 import { Options as SystemOptions } from "../system/system"; /* eslint-disable-line no-unused-vars */ // ESLint type import detection bug
+import { access } from "fs";
 
 interface OptionsVars {
 	homepage: string;
 } /* eslint-disable-line no-extra-semi */ // ESLint inteface no-extra-semi bug
 export interface OptionsInterface extends SystemOptions, OptionsVars {} /* eslint-disable-line no-extra-semi */ // ESLint inteface no-extra-semi bug
-
-/** Stores system options. */
-export default class Options extends Info {
-	constructor({ args, system, publicEntrypoint, protectedEntrypoint, vars }: SubsystemExtensionArgs) {
-		// Set options to be read
-		if (args.system_args !== undefined) {
-			// Assign system option args
-			let { options }: { options: SystemOptions } = args.system_args;
-
-			// Check options failure
-			if (!checkOptionsFailure(options)) {
-				let variables: OptionsInterface = {
-					...options,
-					...(vars as OptionsVars)
-				};
-
-				// Call superclass constructor
-				super({
-					args: new Object() as SubsystemExtensionArgs,
-					protectedEntrypoint,
-					publicEntrypoint,
-					system,
-					vars: variables
-				});
-
-				// Terminate function execution
-				return;
-			}
-		}
-
-		// Report an error
-		throw new LoaderError("system_options_failure", "The options provided to the system constructor are inconsistent.");
-	}
-}
 
 /**
  * Checks options argument for missing incorrect property types
@@ -68,7 +35,7 @@ export default class Options extends Info {
  *   throw new Error ("Options inconsistent.");
  * }
  */
-function checkOptionsFailure(options: SystemOptions) {
+export function checkOptionsFailure(options: SystemOptions) {
 	let failed = false;
 
 	if (options) {
@@ -99,4 +66,47 @@ function checkOptionsFailure(options: SystemOptions) {
 		failed = true;
 	}
 	return failed;
+}
+
+/** Stores system options. */
+export default class Options extends Info {
+	constructor({ args, system, publicEntrypoint, protectedEntrypoint, vars }: SubsystemExtensionArgs) {
+		// Set options to be read
+		if (args.system_args !== undefined) {
+			// Assign system option args
+			let { options }: { options: SystemOptions } = args.system_args;
+
+			// Check options failure
+			if (!checkOptionsFailure(options)) {
+				let variables: OptionsInterface = {
+					...options,
+					...(vars as OptionsVars)
+				};
+
+				// Call superclass constructor
+				super({
+					args: new Object() as SubsystemExtensionArgs,
+					protectedEntrypoint,
+					publicEntrypoint,
+					system,
+					vars: variables
+				});
+
+				// Add method
+				this.addMethods([
+					{
+						access: Access.public | Access.protected | Access.private,
+						fn: checkOptionsFailure,
+						name: "checkOptionsFailure"
+					}
+				]);
+
+				// Terminate function execution
+				return;
+			}
+		}
+
+		// Report an error
+		throw new LoaderError("system_options_failure", "The options provided to the system constructor are inconsistent.");
+	}
 }
